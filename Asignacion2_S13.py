bf0d35265d70bf145c87beb9bcf6a757313ab175
vyadir <yadir.sve@gmail.com>
2023-10-19 19:53:39 -0600

    Asignación e decripción

---
+++ Asignacion2_S13.py
@@ -0,0 +1,10 @@
+"""
+    Creado por 
+    Descripción: 
+        Este grupo se encargará de diseñar el procedimiento que deberá mostrar el programa en LATEX. Para esto buscar 2 ejemplos (sin
+        variables artificiales y con variables artificiales, ambos con solución) y realizar la solución detallada (a pie y en LATEX). Esto deberá ser documentado, 
+        traten de ser lo más didáctico posible sin exagerar el procedimiento. Luego que tengan esto, empezarán a analizar como extraer la información del
+        programa que trabajó el Grupo A para ir construyendo el procedimiento paso por paso.
+    
+            
+"""
