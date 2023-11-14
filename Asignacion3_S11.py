268619b3ad2b49267b526c26606fc6d8f47f0c77
vyadir <yadir.sve@gmail.com>
2023-11-13 18:09:31 -0600

    correccion s3 semana 11

--- Asignacion3_S11.py
+++ Asignacion3_S11.py
@@ -14,7 +14,7 @@ Descripción de la tarea:
 
 """
 
-def Tabla2LaTeX(A):
+def Tabla2LaTeX(A, Ampl):
     m = len(A)      # Calcula el número de filas de la matriz
     n = len(A[0])   # Calcula el número de columnas de la matriz
     # Comienza a construir el código LaTeX
@@ -28,23 +28,22 @@ def Tabla2LaTeX(A):
         # Elimina el último " & " de la fila y agrega una nueva línea
         latex_code = latex_code[:-2] + " \\\\\n"
         # Agrega una línea horizontal antes de la penúltima fila
-        if i == m - 2:
+        if Ampl and i == m - 3:
+            latex_code += "\\hline\n"
+        if not Ampl and i == m - 2:
             latex_code += "\\hline\n"
     # Agrega la línea horizontal final y el cierre de la tabla LaTeX
     latex_code += "\\hline\n\\end{tabular}"
     return latex_code
 
 # Ejemplo de una matriz MxN
-A = [[1,2,31,4,1,3,1,1],
-     [1,2,31,4,1,3,1,2],
-     [1,2,31,4,1,3,1,3],
-     [1,2,31,4,1,3,1,4],
-     [1,2,31,4,1,3,1,5],
-     [1,2,31,4,1,3,1,6],
-     [1,2,31,4,1,3,1,7],
-     [1,2,31,4,1,3,1,"z"]]
+A = [[0.0, 0.0, 2.4285714285714284, 1.0, 0.42857142857142855, 17.0],
+[1.0, 0.0, 1.1428571428571428, 0.0, 0.14285714285714285, 8.0],
+[0.0, 1.0, 0.14285714285714285, 0.0, 0.14285714285714285, 4.0],
+[0.0, 0.0, 2.4285714285714284, 0.0, 0.42857142857142855, 20.0]]
 
 # Convierte la matriz en una tabla LaTeX 
-codigo_latex = Tabla2LaTeX(A)
+codigo_latex = Tabla2LaTeX(A, False)
+
 # Imprime el código LaTeX resultante
 print(codigo_latex)
